4a8342e8a05d0524a3574316bdb58c20a2459cbf
Lucas PREA <lucas.sokha@gmail.com>
2022-12-27 15:43:37 +0100

    modified test.js

--- scripts/pages/test.js
+++ scripts/pages/test.js
@@ -1,90 +1,37 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import { dataApi } from "/scripts/utils/dataApi.js";
 
-// import {photographerFactory} from "/scripts/factories/photographer.js"
-import { getPhotographers, dataPhotographers } from "./index.js";
-// import { dataPhotographers } from "./index.js";
-// setTimeout(() => {
-//   console.log(dataPhotographers);  //photographers list
-// }, 200);
-// console.log('In usingAwait module');
+const { getPhotographer } = dataApi();
 
-// console.log(getPhotographers);
+async function getMediaPathById(photographerId) {
+  // Récupérer les informations sur le photographe à partir de son ID
+  const photographer = await getPhotographer(photographerId);
 
-const displayPhotographer = async (data) => {
-  await getPhotographers();
+  // Récupérer le nom du photographe
+  const photographerName = photographer.name;
+// console.log(photographerName);
+  // Remplacer les espaces dans le nom du photographe par des tirets
+  const normalizedName = photographerName.replace(/ /g, "-");
+// console.log(normalizedName);
+  // Construire le chemin vers le dossier de médias
+  const mediaPath = `assets/medias/${normalizedName}`;
+// console.log(mediaPath);
 
-  // console.log(data);
+  return mediaPath;
+}
 
-  // const dataPhotographers = data.photographers;
-  // const dataPhotographers = data.photographers;
-  function photographerFactory(data) {
-    // const { id, name, city, tagline, price } = data;
-    // console.log(data);
-    const id = new URL(location.href).searchParams.get("id");
-    const photographers = !id
-      ? dataPhotographers
-      : dataPhotographers.filter((photographer) => photographer.id == id);
-    console.log(photographers);
-    const picture = `assets/photographers/${photographers[0].portrait}`;
+export function mediaFactory(data) {
+    const { id, photographerId, title, image, likes, date, price, video } = data;
+    // const imgMedia = `assets/medias/${image}`;
+    // const vMedia = `assets/medias/${video}`;
+  
+    // Appeler la fonction getMediaPathById pour récupérer le chemin vers le dossier de médias
+    // du photographe à partir de son ID
+    const mediaPath = getMediaPathById(photographerId);
+//   console.log(mediaPath);
+  //   // Construire le chemin complet vers le média en ajoutant le nom de fichier à la fin
+    const imgMedia = `${mediaPath}/${image}`;
+    const vMedia = `${mediaPath}/${video}`;
+console.log(imgMedia);
+console.log(vMedia);
 
-
-    function getUserCardDOM() {
-      const article = document.createElement("article");
-      const img = document.createElement("img");
-      img.setAttribute("src", picture);
-      const h2 = document.createElement("h2");
-      h2.textContent = photographers[0].name;
-      const dataCity = document.createElement("h4");
-      dataCity.textContent = photographers[0].city;
-      const dataTagline = document.createElement("p");
-      dataTagline.textContent = photographers[0].tagline;
-      const dataPrice = document.createElement("span");
-      dataPrice.textContent = photographers[0].price + "€/jour";
-
-      article.appendChild(img);
-      article.appendChild(h2);
-      article.appendChild(dataCity);
-      article.appendChild(dataTagline);
-      article.appendChild(dataPrice);
-
-      console.log(photographers[0].id);
-      console.log(picture);
-      console.log(photographers[0].name);
-      console.log(photographers[0].city);
-      console.log(photographers[0].tagline);
-      console.log(photographers[0].price);
-      return article;
-    }
-    getUserCardDOM();
-    return { id, name, picture, city, tagline, price };
-  }
-  photographerFactory();
-  // return {data}
-  // console.log(photographerFactory());
-
-  console.log(displayPhotographer());
-};
-displayPhotographer();
-
-// async function displayD(photographers) {
-//   const photographerHeader = document.querySelector(".photograph-header");
-
-//   photographers.forEach((photographer) => {
-//   const photographerMod = photographerFactory(photographers);
-//   const userCardDOM = photographerMod.getUserCardDOM();
-//   photographerHeader.appendChild(userCardDOM);
-//   displayPhotographer(data)
-//   });
-// }
-
-// async function init() {
-//   // Récupère les datas des photographes
-//   const { photographers } = await getPhotographers();
-//   displayD(photographers);
-
-//   // console.log(photographers);
-// }
-
-init();
-
-/////////////
+}
